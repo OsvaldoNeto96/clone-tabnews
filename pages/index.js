c31adc74795d51e4556635160af40b6172dd185c
Osvaldo Neto <osavldo.narciso96@gmail.com>
2025-10-31 19:49:34 -0300

    modifica a frase de teste feita anteriormente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Estudar e me focar, tem sido uma das tarefas mais difíceis que já enfrentei.</h1>
+    return (
+        <>
+            <h1>Estudar e me focar, tem sido uma das tarefas mais difíceis que já enfrentei.</h1>
+            <h2>Mas eu sei que com esforço e dedicação, posso superar esse desafio.</h2>
+        </>
+    );
 }
 
 export default Home;
