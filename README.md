6f8bbca7da13f9267452aa062476f82385b72972
Osvaldo Neto <osavldo.narciso96@gmail.com>
2025-11-12 20:00:20 -0300

    adiciona os scripts `lint:chech` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprender a utilizar o git, através do curso .dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 function Home() {
-    return (
-        <>
-            <h1>Estudar e me focar, tem sido uma das tarefas mais difíceis que já enfrentei.</h1>
-            <h2>Mas eu sei que com esforço e dedicação, posso superar esse desafio.</h2>
-            <h4>Eu vou continuar tentando, um dia de cada vez.💪🏾</h4>
-        </>
-    );
+  return (
+    <>
+      <h1>
+        Estudar e me focar, tem sido uma das tarefas mais difíceis que já
+        enfrentei.
+      </h1>
+      <h2>
+        Mas eu sei que com esforço e dedicação, posso superar esse desafio.
+      </h2>
+      <h4>Eu vou continuar tentando, um dia de cada vez.💪🏾</h4>
+    </>
+  );
 }
 
 export default Home;
